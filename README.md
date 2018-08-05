cfcb5036f79d44a0187344390fd2603dfc42871b
Binit Shah <bshah@gatech.edu>
2018-08-05 03:15:55 -0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# gtmakery.github.io
-GT Makery's website is where we will update current events, projects, lectures, resources and everything else.
---
+++ path----c15d95409244b06268ea.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"GT Makery"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----c15d95409244b06268ea.js.map
