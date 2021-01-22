88ad94ef13fd13bb3f24d207dc26cf0bb79138bb
Mudit Gupta <muditg317@gmail.com>
2021-01-22 17:53:27 -0500

    rename branch

---
+++ docs/precache-manifest.193df57246dc16a07a0b6f76c3dff9d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41bcd079dbd96df663e335f32007ce5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f5ce9436cef18de74f2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ace4db6c851be9720df",
+    "url": "/static/js/2.e41e5475.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e41e5475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5ce9436cef18de74f2",
+    "url": "/static/js/main.f26f5d64.chunk.js"
+  },
+  {
+    "revision": "119181540b8e072ce06f",
+    "url": "/static/js/runtime-main.caa9650a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
